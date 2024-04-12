257be2aa5810cf3a8c1d0e5e13b27810e9b1ed84
Gourav Jamloki <gourav.jamloki@gmail.com>
2024-04-12 22:53:58 +0530

    Constructors And Its Types

---
+++ 0-OOPsConcept/Constructor.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle
+{
+
+public:
+    int l;
+    int b;
+
+    Rectangle() //Default Constructor
+    {
+        l = 0;
+        b = 0;
+    }
+
+    Rectangle(int x, int y) //Parameterised Constructors
+    {
+        l = x;
+        b = y;
+    }
+
+    Rectangle(Rectangle& r) //Copy Constructor
+    // when defining a copy constructor, it's common practice to pass the object by reference rather than by value. This is done to avoid unnecessary copying of the object, which can be inefficient, especially for large objects.
+    {
+        l = r.l;
+        b = r.b;
+    }
+};
+
+int main()
+{
+    Rectangle r1;
+    cout<<r1.l<<" "<<r1.b<<endl;
+
+    Rectangle r2(3,5);
+    cout<<r2.l<<" "<<r2.b<<endl;
+
+    Rectangle r3 = r2;
+    cout<<r3.l<<" "<<r3.b<<endl;
+
+    return 0;
+}
