be43cce94a4e2d957f772aae6d80bbe517f90f78
Gourav Jamloki <gourav.jamloki@gmail.com>
2024-04-13 23:17:14 +0530

    Constructor And Destructors

--- 0-OOPsConcept/02-Constructor.cpp
+++ 0-OOPsConcept/02-Constructor.cpp
@@ -21,17 +21,26 @@ public:
     }
 
     Rectangle(Rectangle& r) //Copy Constructor
-    // when defining a copy constructor, it's common practice to pass the object by reference rather than by value. This is done to avoid unnecessary copying of the object, which can be inefficient, especially for large objects.
+    // when defining a copy constructor, it's common practice to pass the object by reference rather than by value. 
+    // This is done to avoid unnecessary copying of the object, which can be inefficient, especially for large objects.
     {
         l = r.l;
         b = r.b;
+    }
+
+    ~Rectangle() //Destructor
+
+    // Objects will be deleted after executing all
+    {
+        cout<<"Destructor is called "<<endl;
     }
 };
 
 int main()
 {
-    Rectangle r1;
-    cout<<r1.l<<" "<<r1.b<<endl;
+    Rectangle* r1 = new Rectangle();
+    cout<<r1->l<<" "<<r1->b<<endl;
+    delete r1;
 
     Rectangle r2(3,5);
     cout<<r2.l<<" "<<r2.b<<endl;
